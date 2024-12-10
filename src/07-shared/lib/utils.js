e4b3c146a6e969574a1f3e7210c66c660c96a21c
Yegor <galax.yegor.uaz.s4@gmail.com>
2024-12-10 13:18:16 +0300

    fix: fix id bug

--- src/07-shared/lib/utils.js
+++
@@ -1,9 +0,0 @@
-/**
- * функция генерирует Id из объекта даты
- * @param value - объект даты
- * @returns {string} - Id даты
- *
- */
-export const getDateId = (value) => {
-    return  ''+ value.date()+(value.month() + 1)+value.year();
-}
